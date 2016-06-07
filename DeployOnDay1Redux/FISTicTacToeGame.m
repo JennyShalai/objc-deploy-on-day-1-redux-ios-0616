347f2dd61f28d39895794f5286b8e20abf1d1aa6
JennyShalai <jenny.shalai@gmail.com>
2016-06-07 05:14:33 -0400

    final version, add comments

--- DeployOnDay1Redux/FISTicTacToeGame.m
+++ DeployOnDay1Redux/FISTicTacToeGame.m
@@ -22,8 +22,6 @@
 {
     self = [super init];
     if(self) {
-        // Do initialization of your game here, inside this if statement.
-        // Leave the rest of this method alone :)
         [self resetBoard];
     }
     return self;
@@ -31,96 +29,105 @@
 
 -(void)resetBoard {
     // clear the game board of all player moves
-    _board = [[NSMutableArray alloc] initWithCapacity: 3];
-    [_board insertObject:[NSMutableArray arrayWithObjects:@"",@"",@"",nil] atIndex:0];
-    [_board insertObject:[NSMutableArray arrayWithObjects:@"",@"",@"",nil] atIndex:1];
-    [_board insertObject:[NSMutableArray arrayWithObjects:@"",@"",@"",nil] atIndex:2];
-    _currentPlayer = @"";
+    self.board = [[NSMutableArray alloc] initWithCapacity: 3];
+    [self.board insertObject:[NSMutableArray arrayWithObjects:@"",@"",@"",nil] atIndex:0];
+    [self.board insertObject:[NSMutableArray arrayWithObjects:@"",@"",@"",nil] atIndex:1];
+    [self.board insertObject:[NSMutableArray arrayWithObjects:@"",@"",@"",nil] atIndex:2];
+    self.currentPlayer = @"";
 
 }
 
 -(NSString *)playerAtColumn:(NSUInteger)column row:(NSUInteger)row {
     // determine what player has a piece at the given position.
-    // It should return a string representing which player (if any) has a piece at the given position.
-    // Valid return values are "X", "O", and "" (the empty string, for a space with no piece).
-    NSLog(@"playerAtColumn");
-    if ([_board[row][column] isEqual: @"X"]) {
+    // It should return a string representing which player has a piece at the given position.
+    // Valid return values are "X", "O", and ""
+    if ([self.board[row][column] isEqual: @"X"]) {
         return @"X";
-    } else if ([_board[row][column] isEqual: @"O"]) {
+    } else if ([self.board[row][column] isEqual: @"O"]) {
         return @"O";
     } else return @"";
 }
 
 -(BOOL)canPlayAtColumn:(NSUInteger)column row:(NSUInteger)row {
     // return YES if the given position is a valid place to play a piece, and NO if not
-    NSLog(@"canPlayAtColumn");
-    if (_board[row][column] == nil || [_board[row][column]  isEqual: @""]) {
+    if (self.board[row][column] == nil || [self.board[row][column]  isEqual: @""]) {
         return YES;
     } else {
-    return NO;
+        return NO;
     }
 }
 
 -(void)playXAtColumn:(NSUInteger)column row:(NSUInteger)row {
-    NSLog(@"playXAtColumn");
-    _board[row][column] = @"X";
-    _currentPlayer = @"X";
+    self.board[row][column] = @"X";
+    self.currentPlayer = @"X";
 }
 
 -(void)playOAtColumn:(NSUInteger)column row:(NSUInteger)row {
-    NSLog(@"playOAtColumn");
-    _board[row][column] = @"O";
-    _currentPlayer = @"O";
+    self.board[row][column] = @"O";
+    self.currentPlayer = @"O";
 }
 
 -(NSString *)winningPlayer {
-    // called after every turn is completed.
-    // It should check if the board currently has a winner, and return "X", "O", or ""
+    // called after every turn is completed
+    // it should check if the board currently has a winner, and return "X", "O", or ""
     // the empty string, meaning no winner
-    NSLog(@"winningPlayer");
     
-    if ((_board[0][0] == _board[0][1]) && (_board[0][0] == _board[0][2])) {
-        if (![_board[0][0] isEqualToString:@""]) {
-            return _currentPlayer;
+    // top row
+    if (([self.board[0][0] isEqualToString:self.board[0][1]])
+     && ([self.board[0][0] isEqualToString:self.board[0][2]])
+     && (![self.board[0][0] isEqualToString:@""]) ) {
+            return self.currentPlayer;
+        
+        // middle row
+        } else if ([self.board[0][1] isEqualToString:self.board[1][1]]
+               && ([self.board[0][1] isEqualToString:self.board[2][1]])
+               && (![self.board[0][1] isEqualToString:@""])) {
+                    return self.currentPlayer;
+            
+        // bottom row
+        } else if (([self.board[0][2] isEqualToString:self.board[1][2]])
+               && ([self.board[0][2] isEqualToString:self.board[2][2]])
+               && (![self.board[0][2] isEqualToString:@""])) {
+                    return self.currentPlayer;
+            
+        // left column
+        } else if (([self.board[0][0] isEqualToString:self.board[1][0]])
+               && ([self.board[0][0] isEqualToString:self.board[2][0]])
+               && (![self.board[0][0] isEqualToString:@""])) {
+                    return self.currentPlayer;
+            
+        // middle column
+        } else if (([self.board[1][0] isEqualToString:self.board[1][1]])
+               && ([self.board[1][0] isEqualToString:self.board[1][2]])
+               && (![self.board[1][0] isEqualToString:@""])) {
+                    return self.currentPlayer;
+            
+        // right column
+        } else if (([self.board[2][0] isEqualToString:self.board[2][1]])
+               && ([self.board[2][0] isEqualToString:self.board[2][2]])
+               && (![self.board[2][0] isEqualToString:@""])) {
+                    return self.currentPlayer;
+            
+        // diagonal
+        } else if (([self.board[0][0] isEqualToString:self.board[1][1]])
+               && ([self.board[0][0] isEqualToString:self.board[2][2]])
+               && (![self.board[0][0] isEqualToString:@""])) {
+                    return self.currentPlayer;
+            
+        // diagonal
+        } else if (([self.board[2][0] isEqualToString:self.board[1][1]])
+               && ([self.board[0][2] isEqualToString:self.board[1][1]])
+               && (![self.board[2][0] isEqualToString:@""])) {
+                    return self.currentPlayer;
         }
-    } else if ([_board[0][1] isEqualToString:_board[1][1]] && (_board[0][1] == _board[2][1])) {
-        if (![_board[0][1] isEqualToString:@""]) {
-            return _currentPlayer;
-        }
-    } else if ((_board[0][2] == _board[1][2]) && (_board[0][2] == _board[2][2])) {
-        if (![_board[0][2] isEqualToString:@""]) {
-            return _currentPlayer;
-        }
-    } else if ((_board[0][0] == _board[0][1]) && (_board[0][0] == _board[0][2])) {
-        if (![_board[0][0] isEqualToString:@""]) {
-            return _currentPlayer;
-        }
-    } else if ((_board[1][0] == _board[1][1]) && (_board[1][0] == _board[1][2])) {
-        if (![_board[1][0] isEqualToString:@""]) {
-            return _currentPlayer;
-        }
-    } else if ((_board[2][0] == _board[2][1]) && (_board[2][0] == _board[2][2])) {
-        if (![_board[2][0] isEqualToString:@""]) {
-            return _currentPlayer;
-        }
-    } else if ((_board[0][0] == _board[1][1]) && (_board[0][0] == _board[2][2])) {
-        if (![_board[0][0] isEqualToString:@""]) {
-            return _currentPlayer;
-        }
-    } else if ((_board[2][0] == _board[1][1]) && (_board[0][2] == _board[1][1])) {
-        if (![_board[2][0] isEqualToString:@""]) {
-            return _currentPlayer;
-        }
-    }
     return @"";
 }
 
 -(BOOL)isADraw {
-    NSLog(@"isADraw");
     NSString *win = [self winningPlayer];
     BOOL SpaceToMove = NO;
     
-    for (NSArray *element in _board) {
+    for (NSArray *element in self.board) {
         if ([element containsObject:@""]) {
             SpaceToMove = YES;
         }
